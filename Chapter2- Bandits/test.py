1f8fca11c6003d961893ee5e3ccaa136bf9a7075
Kyle Clarks <kyleclarkson17@hotmail.com>
2019-09-04 22:44:51 -0700

    Added plotting to file.

--- Chapter2- Bandits/test.py
+++ Chapter2- Bandits/test.py
@@ -1,13 +1,17 @@
 import random as rd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 # A run is a single instance of a k-armed bandit
-num_of_runs = 100
-# A pull (time step) is the chooseing of an action, specified by the action selection rule.
-num_of_pulls = 2000
-# The number of actions (arms) in which one is choosen per pull.
+num_of_runs = 2000
+
+# A pull is the decision of which arm to pull.
+num_of_pulls = 4000
+
+# The number of arms available to be pulled.
 num_of_arms = 10
+
 # For each run, sample the true reward for each arm.
 q_true = np.random.normal(0, 1, (num_of_runs, num_of_arms))
 
@@ -15,12 +19,14 @@ q_true = np.random.normal(0, 1, (num_of_runs, num_of_arms))
 true_opt_arms = np.argmax(q_true, 1)
 
 # The various epsilon values for the epsilon greedy strategy
-epsilon_values = [0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 0.75, 0.9, 1] #10 difference values
+epsilon_values = [0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 0.8, 0.9, 1]
 
-# Run epsilon-greedy stratergy
+# Set up plots for graphing rewards.
+fig1 = plt.figure().add_subplot(111)
+fig2 = plt.figure().add_subplot(111)
 
-for epsilon_i in range(len(epsilon_values)):
-    print("Running epsilon value: ", epsilon_values[epsilon_i])
+# Run epsilon-greedy strategy for each value.
+for eps_i in range(len(epsilon_values)):
 
     # Reward estimate
     Q = np.zeros((num_of_runs, num_of_arms))
@@ -31,19 +37,19 @@ for epsilon_i in range(len(epsilon_values)):
     # Initially pull all arms
     Q_init = np.random.normal(q_true, 1)
 
-    # Set reward array.
-    R_ep = []
-    R_ep.append(0)
-    R_ep.append(np.mean(Q_init))
-    R_ep_opt = []
+    # Reward for this epsilon run.
+    reward_eps = []
+    reward_eps.append(0)
+    # reward_eps.append(np.mean(Q_init))
+    reward_eps_opt = []
 
-    for pull in range(2, num_of_pulls + 1):
-        R_pull = []
+    for pull in range(1, num_of_pulls + 1):
+        reward_pull = []
         opt_arm_pull = 0  # Number of pulls of for the best arm (in this time step)
 
         for i in range(num_of_runs):
             # Choice arm uniformly at random
-            if rd.random() < epsilon_values[epsilon_i]:
+            if rd.random() < epsilon_values[eps_i]:
                 arm = np.random.randint(num_of_arms)
             # Choose best arm.
             else:
@@ -56,16 +62,32 @@ for epsilon_i in range(len(epsilon_values)):
             # Reward is estimated by sampling around the true action-value.
             reward_i = np.random.normal(q_true[i][arm], 1)
 
-            R_pull.append(reward_i)
+            reward_pull.append(reward_i)
             N[i][arm] += 1
             Q[i][arm] = Q[i][arm] + (reward_i - Q[i][arm]) / N[i][arm]
 
         # == End runs ==
-        average_R_pull = np.mean(R_pull)
-        R_ep.append(average_R_pull)
-        R_ep_opt.append(float(opt_arm_pull) * 100 / num_of_pulls)
+        # Append average reward to rewards list.
+        average_reward_pull = np.mean(reward_pull)
+        reward_eps.append(average_reward_pull)
+        # Percentage optimal arm was pulled.
+        reward_eps_opt.append(float(opt_arm_pull) * 100 / num_of_pulls)
+
+    fig1.plot(range(0, num_of_pulls + 1), reward_eps, label="$\epsilon$="+str(epsilon_values[eps_i]))
+    fig2.plot(range(1, num_of_pulls + 1), reward_eps_opt, label="$\epsilon$="+str(epsilon_values[eps_i]))
 
     # == End pulls ==
-    print("Average reward: ", np.average(R_ep))
+    print("Epsilon value: ", epsilon_values[eps_i], "Average reward: ", np.average(reward_eps))
+
+fig1.title.set_text('$\epsilon$-greedy : Average Reward vs Number of Pulls')
+fig1.set_xlabel("Pulls")
+fig1.set_ylabel("Average Reward")
+fig1.legend(loc="best")
+
+fig2.title.set_text("$\epsilon$-greedy : Percentage of Optimal Action vs Number of Pulls")
+fig2.set_xlabel("Pulls")
+fig2.set_ylabel("Percentage of Optimal Action")
+fig2.legend(loc="best")
+plt.show()
 
 
