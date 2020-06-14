2608c6a88014d919e59e43f96e32d222f28bc26f
kyle clarkson <clarkson@ubc.ca>
2020-06-14 13:34:22 -0700

    Restructured project.

---
+++ learning-methods/QLearning.py
@@ -0,0 +1,19 @@
+
+
+class TrainingConfig():
+    '''
+    This class contains the hyperparameter values used by the learning algorithm.
+
+    '''
+    pass
+
+class QLearning():
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+
+
+
