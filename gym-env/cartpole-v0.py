f33ff2647bad3120bb8418b8d58266ebef3de51d
kyle clarkson <clarkson@ubc.ca>
2020-09-08 22:28:24 -0700

    Added TD Q-Learning for CartPole Example

--- gym-env/cartpole-v0.py
+++ gym-env/cartpole-v0.py
@@ -1,20 +1,83 @@
 import gym
+import numpy as np
+import matplotlib.pyplot as plt
 
-env = gym.make('CartPole-v0')
+"""
+    A Temporal Difference Q Learning 
+    approach for CartPole environment.
+"""
 
-for eps in range(20):
 
-    obs = env.reset()
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
 
-    for t in range(1000):
+    alpha = 0.05
+    gamma = 0.95
 
-        env.render()
-        action = env.action_space.sample()
 
-        o, r, done, info = env.step(action)
+    states = np.linespace()
 
-        if done:
-            print("Episode finished after {} steps".format(t+1))
-            break
+class Agent():
 
-env.close()
+    def __init__(self,
+                 lr,
+                 gamma,
+                 n_actions,
+                 state_space,
+                 epsil_start,
+                 epsil_end,
+                 epsil_dec):
+
+        self.lr = lr
+        self.gamma = gamma
+        self.epsilon = epsil_start
+        self.epsil_end = epsil_end
+        self.epsil_dec = epsil_dec
+
+        self.n_actions = n_actions
+        self.state_space = state_space
+
+        self.actions = [i for i in range(self.n_actions)]
+
+        self.Q = {}
+
+        self.init_Q()
+
+    def init_Q(self):
+        """ Init all Q value pairs to 0. """
+        for state in self.state_space:
+            for action in self.actions:
+                self.Q[(state, action)] = 0
+
+    def max_action(self, state):
+        """ Return action corresponding to max Q(s,a). """
+        actions = np.array([self.Q[(state, a)] for a in self.actions])
+        action = np.argmax(actions)
+        return action
+
+    def choose_action(self, state):
+        """ Use epsilon greedy to choose action. """
+        if np.random.random() < self.epsilon:
+            action = np.random.choice(self.actions)
+        else:
+            action = self.max_action(state)
+
+        return action
+
+    def decrease_epsilon(self):
+        if self.epsilon > self.epsil_end:
+            self.epsilon -= self.epsil_dec
+        else:
+            self.epsilon = self.epsil_end
+
+    def update_Q(self, state, action, reward, state_):
+        """ Update Q values using TD update rule. """
+        a_max = self.max_action(state_)
+
+        self.Q[(state, action)] = self.Q[(state, action)] +\
+                                  self.lr*(reward + self.gamma*self.Q[(state, a_max)]-self.Q[(state,action)])
+
+
+
+
+
